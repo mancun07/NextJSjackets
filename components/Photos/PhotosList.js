0756fb386e10ec60e9fa1ae85dd12b3e98e4d61f
Dima <address>
2021-08-14 22:10:24 +0300

    a new commit

--- components/Photos/PhotosList.js
+++ components/Photos/PhotosList.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {Fragment, useState, useRef, useEffect} from 'react'
 import classes from './PhotosList.module.scss'
-import {motion} from 'framer-motion'
+import {motion, useDomEvent} from 'framer-motion'
 import PropTypes from 'prop-types';
+import Image from 'next/image'
 
 
 const PhotosList = (props) => {
+
+       const [overlayOpen, setOverlayOpen] = useState(false)
+       const [imageSrc, setImageSrc] = useState('')
+
+       const showOverlay = (id) => {      
+            let imageEl = props.photos.find(el => el.id === id)
+            let imgSrc = imageEl.src
+            setImageSrc(imgSrc)
+            setOverlayOpen(true)  
+       }
+
+       const closeOverlay = () => {
+            setImageSrc('')
+            setOverlayOpen(false)  
+       }
+
     return (
+        <Fragment>
         <div className={classes['photos-list']}>
             {props.photos.map(el => {
-                return <motion.img
+                return <div key={el.id} onClick={() => showOverlay(el.id)} >
+                <motion.img
+                src={el.src} 
+                alt={el.title}
                 initial={{x: '-100vw'}}
                 animate={{x: 0}}
                 transition={{duration: 1, type: 'spring'}}
@@ -17,13 +38,17 @@ const PhotosList = (props) => {
                     rotateZ: 5,
                     boxShadow: "0px 0px 8px rgb(255,255,255)"  
                 }}
-               
-                key={el.id} 
-                src={el.src} 
-                alt={el.title}
                 />
+                </div>
             })}
         </div>
+        {overlayOpen && <div className={classes.imageContainer}>
+                 <Image src={imageSrc} width="200" height="200"
+                  layout="responsive"
+                  priority={true}/>
+        </div>}
+        {overlayOpen && <div className={classes.close} onClick={closeOverlay}>X</div>}
+        </Fragment>
     )
 }
 
--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PhotosList from '../../components/Photos/PhotosList'
+import { v4 } from "uuid";
 
 const dummyPhotos = [
-    {id: '1', title: 'Band Photo1', src:'./about1.jpg'},
-    {id: '2', title: 'Band Photo2', src:'./about2.jpg'},
-    {id: '3', title: 'Band Photo3', src:'./about3.jpg'},
-    {id: '4', title: 'Band Photo4', src:'./about4.jpg'},
-    {id: '5', title: 'Band Photo5', src:'./about5.jpg'},
-    {id: '6', title: 'Band Photo6', src:'./about6.jpg'},
-    {id: '7', title: 'Band Photo7', src:'./concert1.jpg'},
-    {id: '8', title: 'Band Photo8', src:'./concert2.jpg'},
-    {id: '9', title: 'Band Photo9', src:'./concert3.jpg'}
+    {id: v4(), title: 'Band Photo1', src:'/about1.jpg'},
+    {id: v4(), title: 'Band Photo2', src:'/about2.jpg'},
+    {id: v4(), title: 'Band Photo3', src:'/about3.jpg'},
+    {id: v4(), title: 'Band Photo4', src:'/about4.jpg'},
+    {id: v4(), title: 'Band Photo5', src:'/about5.jpg'},
+    {id: v4(), title: 'Band Photo6', src:'/about6.jpg'},
+    {id: v4(), title: 'Band Photo7', src:'/concert1.jpg'},
+    {id: v4(), title: 'Band Photo8', src:'/concert2.jpg'},
+    {id: v4(), title: 'Band Photo9', src:'/concert3.jpg'}
   ]
 
 const PhotosPage = () => {
